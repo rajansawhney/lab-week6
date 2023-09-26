c427a90aa39e5d2d592b8f94eb9ee6e0655ead5f
rajansawhney <rajan.k.sawhney@gmail.com>
2023-09-26 10:29:22 -1000

    comment update

--- primes-in-range.c
+++ primes-in-range.c
@@ -1,45 +1,17 @@
 /*
 Write a C Program that finds and prints all prime numbers in an interval between two integers.
+Interval numbers are not inclusive.
 
 NOTE:
 1. Must use at least 2 functions ( one being main() )
 2. Accept only positive integers
 3. Handle scenario where first number (n1) is larger than second number (n2)
-4. Print all the prime numbers separated by a space in one line
+4. Display output in the following format only:
+    Prime numbers between <num1> and <num2> are:
+    <print all the prime numbers separated by ONE SPACE in one line (not including interval numbers)>
+
+Eg:
+Prime numbers between 12 and 55 are:
+13 17 19 23 29 31 37 41 43 47 53
 
 */
-#include <stdio.h>
-
-int checkPrimeNumber(int n);
-
-int main() {
-    int n1, n2, i, flag;
-    printf("Enter two positive integers: ");
-
-    scanf("%d %d", &n1, &n2);
-    // swap n1 and n2 if n1 > n2
-    if (n1 > n2) {
-        n1 = n1 + n2; n2 = n1 - n2; n1 = n1 - n2;
-    }
-    printf("Prime numbers between %d and %d are: ", n1, n2);
-    for (i = n1 + 1; i < n2; ++i) {
-        // flag will be equal to 1 if i is prime
-        flag = checkPrimeNumber(i);
-        if (flag == 1) { 
-            printf("%d ", i);
-        } 
-    }
-    return 0;
-}
-
-// user-defined function to check prime number
-int checkPrimeNumber(int n) {
-    int j, flag = 1;
-    for (j = 2; j <= n / 2; ++j) {
-        if (n % j == 0) { 
-            flag = 0; 
-            break;
-        } 
-    }
-    return flag;
-}
