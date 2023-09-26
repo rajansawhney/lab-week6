9c02ff2e1f4382b0aca9947eb87e08dff1e61d4e
rajansawhney <rajan.k.sawhney@gmail.com>
2023-09-25 14:06:33 -1000

    output format update

--- primes-in-range.c
+++ primes-in-range.c
@@ -5,6 +5,6 @@ NOTE:
 1. Must use at least 2 functions ( one being main() )
 2. Accept only positive integers
 3. Handle scenario where first number (n1) is larger than second number (n2)
-4. Print all the prime numbers separated by comma in one line
+4. Print all the prime numbers separated by a space in one line
 
 */
